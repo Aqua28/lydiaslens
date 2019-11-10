efee8791345b1155e6f047c496cc50f238b6d3b1
Lydia Liu <aidyl889@gmail.com>
2019-11-09 17:51:07 -0800

    better abstracted js file

---
+++ movement.js
@@ -0,0 +1,40 @@
+
+//Google Analytics
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-96537069-1', 'auto');
+    ga('send', 'pageview');
+
+//Google Analytics
+
+
+
+//Slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3500); // Change image every 2 seconds
+}
+
+//Slideshow
+
+
+
+const domContainer = document.querySelector('#gallery_picture');
+ReactDOM.render(e(GalleryPicture), domContainer);
